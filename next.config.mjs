bbccb2c2a596686a3dd2b4a4b0068615454e1b8a
abhij1306 <abhij1306@gmail.com>
2025-12-07 21:32:38 +0530

    fix: remove redundant assetPrefix to resolve production styles

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
   output: 'export',
   basePath: isProd ? '/cube27-website' : '',
-  assetPrefix: isProd ? '/cube27-website/' : '',
   reactStrictMode: true,
   trailingSlash: true,
   images: {
